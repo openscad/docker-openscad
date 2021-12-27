9507c013ce6dc01f766c5bd2e04c8f86520a0481
Torsten Paul <Torsten.Paul@gmx.de>
2021-12-27 19:18:23 +0100

    Add images for OpenSCAD use based on Debian.

---
+++ scripts/build-images.sh
@@ -0,0 +1,36 @@
+#!/bin/bash -xe
+
+build () {
+	REF="$1"
+	BRANCH="$2"
+	DOCKERTAG="$3"
+	VERSION="$4"
+	DIR="$5"
+	FILE="$6"
+
+	if [ -n "$FILE" ]; then
+		DOCKERFILE_ARGS="-f ${DIR}/${FILE}"
+	fi
+
+	if [ -z "$VERSION" ]; then
+		VERSION="$DOCKERTAG"
+	fi
+
+	echo "Building $BRANCH with version $VERSION..."
+	docker build \
+		-t openscad/openscad:"$DOCKERTAG" \
+		--build-arg=REFS="$REF" \
+		--build-arg=BRANCH="$BRANCH" \
+		--build-arg OPENSCAD_VERSION="$VERSION" \
+		$DOCKERFILE_ARGS \
+		"$DIR"
+}
+
+V=$(git log -1 --date="format:%Y.%m.%d.dd%j%H" --format="%cd")
+
+#build	tags	openscad-2015.03	2015.03	""	openscad/buster
+build	tags	openscad-2019.05	2019.05	""	openscad/buster
+build	tags	openscad-2021.01	2021.01	""	openscad/buster
+build	heads	master			dev	"$V"	openscad/bookworm
+
+docker tag openscad/openscad:2021.01 openscad/openscad:latest
