d39f5a552b73eeba8d5af5ebc6de15304832b19f
Torsten Paul <Torsten.Paul@gmx.de>
2023-03-18 02:29:17 +0100

    Set number of jobs based on available CPUs.

--- scripts/build.sh
+++ scripts/build.sh
@@ -13,12 +13,13 @@ build () {
 
 	COUNT=$(( $COUNT + 1 ))
 	LOG=$(printf "%03d_build-%s.log" "$COUNT" "$NAME")
+	JOBS=$(( ( $(nproc --all) + 1 ) / 2 ))
 
 	echo "*" | tee "$LOG"
-	echo "* $(date): building $NAME" | tee -a "$LOG"
+	echo "* $(date): building $NAME (using JOBS=$JOBS)" | tee -a "$LOG"
 	echo "* $(date): log in $LOG" | tee -a "$LOG"
 	echo "*" | tee -a "$LOG"
-	time docker build -t "$TAG":latest --build-arg=JOBS=8 "$@" "$DIR"/"$NAME"/ 2>&1 >> "$LOG"
+	time docker build -t "$TAG":latest --build-arg="JOBS=$JOBS" "$@" "$DIR"/"$NAME"/ 2>&1 >> "$LOG"
 	echo "*" | tee -a "$LOG"
 	echo "* $(date): finished $NAME" | tee -a "$LOG"
 	echo "*" | tee -a "$LOG"
