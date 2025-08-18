8470d5e11369bb055d0b1ee42201f6d006ce6916
Torsten Paul <Torsten.Paul@gmx.de>
2025-08-18 23:11:22 +0200

    Update MXE build.
    * Disable 32-bit build.
    * Handle local build with buildkit.
    * Support MXE update, include CGAL 6.0.1 and Harfbuzz 11.4.1.

--- scripts/build-mxe.sh
+++ scripts/build-mxe.sh
@@ -6,19 +6,27 @@ COUNT=0
 
 . "${SCRIPT_DIR}/build.sh"
 
+context () {
+	TAG="$1"
+	OCI=".oci.${TAG//\//-}"
+	echo "${TAG}=oci-layout://${PWD}/${OCI}"
+}
+
 clean
 
-build mxe openscad/mxe-requirements --no-cache "$@"
+#NO_CACHE=--no-cache
+
+build mxe openscad/mxe-requirements $NO_CACHE "$@"
 
 # Base build for both 32-bit and 64-bit gcc
 build mxe openscad/mxe-base "$@"
 
-build mxe openscad/mxe-x86_64-deps "$@"
-build mxe openscad/mxe-x86_64-gui "$@"
-build mxe openscad/mxe-x86_64-openscad "$@"
+build mxe openscad/mxe-x86_64-deps --build-context $(context openscad/mxe-base) "$@"
+build mxe openscad/mxe-x86_64-gui --build-context $(context openscad/mxe-x86_64-deps) "$@"
+build mxe openscad/mxe-x86_64-openscad --build-context $(context openscad/mxe-x86_64-gui) "$@"
 
-build mxe openscad/mxe-i686-deps "$@"
-build mxe openscad/mxe-i686-gui "$@"
-build mxe openscad/mxe-i686-openscad "$@"
+#build mxe openscad/mxe-i686-deps "$@"
+#build mxe openscad/mxe-i686-gui "$@"
+#build mxe openscad/mxe-i686-openscad "$@"
 
 list 'openscad/mxe-*'
--- scripts/build.sh
+++ scripts/build.sh
@@ -11,15 +11,27 @@ build () {
 	shift
 	shift
 
+	OCI=".oci.${TAG//\//-}"
 	COUNT=$(( $COUNT + 1 ))
 	LOG=$(printf "%03d_build-%s.log" "$COUNT" "$NAME")
-	JOBS=$(( ( $(nproc --all) + 1 ) / 2 ))
+	#JOBS=$(( ( $(nproc --all) + 1 ) / 2 ))
+	JOBS=2
 
 	echo "*" | tee "$LOG"
 	echo "* $(date): building $NAME (using JOBS=$JOBS)" | tee -a "$LOG"
 	echo "* $(date): log in $LOG" | tee -a "$LOG"
 	echo "*" | tee -a "$LOG"
-	time DOCKER_BUILDKIT=1 BUILDKIT_PROGRESS=plain docker build --progress=plain -t "$TAG":latest --build-arg="JOBS=$JOBS" "$@" -f "$DIR"/"$NAME"/Dockerfile "$DIR" 2>&1 >> "$LOG"
+	time DOCKER_BUILDKIT=1 BUILDKIT_PROGRESS=plain docker buildx build \
+		"$DIR" \
+		"$@" \
+		-t "$TAG":latest \
+		--progress=plain \
+		--build-arg="JOBS=$JOBS" \
+		-f "$DIR"/"$NAME"/Dockerfile \
+		--output=type=oci,tar=false,dest="${OCI}" \
+		--output=type=oci,tar=true,dest="${OCI}.tar" \
+		2>&1 | tee -a "$LOG"
+	time docker load -i "${OCI}.tar" 2>&1 | tee -a "$LOG"
 	echo "*" | tee -a "$LOG"
 	echo "* $(date): finished $NAME" | tee -a "$LOG"
 	echo "*" | tee -a "$LOG"
